564c70c1b69fce4837ecf0431f0ce507cfb8874f
ana <ctw04135@criticaltechworks.com>
2025-02-27 13:50:04 +0000

    second commit

--- src/test/java/com/ctw/workstation/services/TeamServiceTest.java
+++ src/test/java/com/ctw/workstation/services/TeamServiceTest.java
@@ -18,53 +18,53 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-@QuarkusTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class TeamServiceTest {
-
-    //When we need to have other dependencies
-
-    @Inject
-    TeamService teamService;
-
-    @InjectMock
-    TeamRepository teamRepository;
-
-    @BeforeAll
-    static void setUp() {
-//        TeamRepository teamRepository = Mockito.mock(TeamRepository.class);
-//        QuarkusMock.installMockForType(teamRepository, TeamRepository.class);
-    }
-
-    @Test
-    @DisplayName("Creating a team")
-    void create_team_throws_exception() {
-        TeamRepository teamRepositoryNew = Mockito.mock(TeamRepository.class);
-        QuarkusMock.installMockForInstance(teamRepositoryNew, teamRepository);
-        Mockito.doThrow(new IllegalArgumentException()).when(teamRepositoryNew).persist(Mockito.any(Team.class));
-        //given
-        TeamRequest teamRequest = new TeamRequest("Dream team","Car","Lisbon");
-        //then
-        assertThatThrownBy(() -> teamService.createTeam(teamRequest)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Not possible to create a team");
-    }
-
-    @Test
-    @DisplayName("Creating a team")
-    void create_team() {
-        //given
-        TeamRequest teamRequest = new TeamRequest("Dream team","Car","Lisbon");
-        //when
-        TeamResponse teamResponse = teamService.createTeam(teamRequest);
-        //then
-        Assertions.assertThat(teamResponse)
-                .as("Response returned with no null values")
-                .hasNoNullFieldsOrProperties()
-                .usingRecursiveComparison()
-                .ignoringFields("id","memberList")
-                .as("Response was returned with the data provided")
-                .isEqualTo(teamRequest);
-
-        Assertions.assertThat(teamRepository.findById(teamResponse.getId())).isNotNull();
-    }
-}
+//@QuarkusTest
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//class TeamServiceTest {
+//
+//    //When we need to have other dependencies
+//
+//    @Inject
+//    TeamService teamService;
+//
+//    @InjectMock
+//    TeamRepository teamRepository;
+//
+//    @BeforeAll
+//    static void setUp() {
+////        TeamRepository teamRepository = Mockito.mock(TeamRepository.class);
+////        QuarkusMock.installMockForType(teamRepository, TeamRepository.class);
+//    }
+//
+//    @Test
+//    @DisplayName("Creating a team")
+//    void create_team_throws_exception() {
+//        TeamRepository teamRepositoryNew = Mockito.mock(TeamRepository.class);
+//        QuarkusMock.installMockForInstance(teamRepositoryNew, teamRepository);
+//        Mockito.doThrow(new IllegalArgumentException()).when(teamRepositoryNew).persist(Mockito.any(Team.class));
+//        //given
+//        TeamRequest teamRequest = new TeamRequest("Dream team","Car","Lisbon");
+//        //then
+//        assertThatThrownBy(() -> teamService.createTeam(teamRequest)).isInstanceOf(IllegalArgumentException.class)
+//                .hasMessage("Not possible to create a team");
+//    }
+//
+//    @Test
+//    @DisplayName("Creating a team")
+//    void create_team() {
+//        //given
+//        TeamRequest teamRequest = new TeamRequest("Dream team","Car","Lisbon");
+//        //when
+//        TeamResponse teamResponse = teamService.createTeam(teamRequest);
+//        //then
+//        Assertions.assertThat(teamResponse)
+//                .as("Response returned with no null values")
+//                .hasNoNullFieldsOrProperties()
+//                .usingRecursiveComparison()
+//                .ignoringFields("id","memberList")
+//                .as("Response was returned with the data provided")
+//                .isEqualTo(teamRequest);
+//
+//        Assertions.assertThat(teamRepository.findById(teamResponse.getId())).isNotNull();
+//    }
+//}
--- src/test/java/com/ctw/workstation/team/boundary/TeamResourceTest.java
+++ src/test/java/com/ctw/workstation/team/boundary/TeamResourceTest.java
@@ -29,102 +29,102 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
-@QuarkusTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@QuarkusTestResource(CtwAcademyResource.class)
-//@TestProfile(CtwAcademyTestProfile.class)
-//@TestHTTPEndpoint(TeamResource.class)
-class TeamResourceTest {
-    @Inject
-    TeamRepository teamRepository;
-
-    private static Long testTeamId;
-
-
-    @BeforeAll
-    void setUpTestData() {
-        Team team = new Team.Builder().setProduct("Test Product").setName("Test Team").setDefaultLocation("Location").build();
-
-        teamRepository.persistAndFlush(team);
-        testTeamId = team.getId();
-    }
-
-    @AfterAll
-    void tearDownTestData() {
-        Team team = teamRepository.findById(testTeamId);
-        if (team != null) {
-            teamRepository.delete(team);
-        }
-    }
-
-    @Test
-    @DisplayName("Fetching all teams")
-    void fetchAllTeams() {
-        Team team = teamRepository.findById(testTeamId);
-        given().header("Content-Type", "application/json")
-                .when().get("/workstation/teams")
-                .then().statusCode(200)
-                .body("$.size()", equalTo(1))
-                .body("[0].name", equalTo(team.getName()))
-                .body("[0].defaultLocation", equalTo(team.getDefaultLocation()))
-                .body("[0].product", equalTo(team.getProduct()));
-    }
-
-    @Test
-    @DisplayName("Fetching a specific team")
-    void fetchSpecificTeam() {
-        Team team = teamRepository.findById(testTeamId);
-        given().header("Content-Type", "application/json")
-                .when().get("/workstation/teams/{id}", testTeamId)
-                .then().statusCode(200)
-                .body("name", equalTo(team.getName()))
-                .body("product", equalTo(team.getProduct()))
-                .body("defaultLocation", equalTo(team.getDefaultLocation()));
-
-        given().header("Content-Type", "application/json")
-                .when().get()
-                .then().statusCode(404);
-    }
-
-    @Test
-    @DisplayName("Creating a team")
-    void createTeam() {
-        TeamRequest teamRequest = new TeamRequest("Default Team", "Product", "Location");
-        TeamResponse teamResponse = new TeamResponse(null,"Default Team", "Location", "Product", null);
-        TeamResponse response = given()
-                .contentType(ContentType.JSON)
-                .body(teamRequest)
-                .when()
-                .post("/workstation/teams")
-                .then()
-                .assertThat()
-                .statusCode(RestResponse.StatusCode.CREATED)
-                .extract().body().as(TeamResponse.class);
-
-        assertThat(response)
-                .as("Team was created")
-                .hasNoNullFieldsOrProperties()
-                .usingRecursiveComparison()
-                .ignoringFields("id", "memberList")
-                .isEqualTo(teamResponse);
-    }
-
-    @Test
-    @DisplayName("Creating a team with blank values")
-    void createTeamWithBlankValues() {
-        TeamRequest teamRequest = new TeamRequest("", "", "");
-        //TODO Criar class de excepção e mapear as respostas de erro para lá.
-        given()
-                .contentType(ContentType.JSON)
-                .body(teamRequest)
-                .when()
-                .post("/workstation/teams")
-                .then()
-                .statusCode(RestResponse.StatusCode.BAD_REQUEST)
-                .body("title", equalTo("Constraint Violation"))
-                .body("violations[0].field", equalTo("create.teamRequest.product"))
-                .body("violations[0].message", equalTo("must not be blank"))
-                .body("violations[1].field", equalTo("create.teamRequest.name"))
-                .body("violations[1].message", equalTo("must not be blank"));
-    }
-}
+//@QuarkusTest
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+////@QuarkusTestResource(CtwAcademyResource.class)
+////@TestProfile(CtwAcademyTestProfile.class)
+////@TestHTTPEndpoint(TeamResource.class)
+//class TeamResourceTest {
+//    @Inject
+//    TeamRepository teamRepository;
+//
+//    private static Long testTeamId;
+//
+//
+//    @BeforeAll
+//    void setUpTestData() {
+//        Team team = new Team.Builder().setProduct("Test Product").setName("Test Team").setDefaultLocation("Location").build();
+//
+//        teamRepository.persistAndFlush(team);
+//        testTeamId = team.getId();
+//    }
+//
+//    @AfterAll
+//    void tearDownTestData() {
+//        Team team = teamRepository.findById(testTeamId);
+//        if (team != null) {
+//            teamRepository.delete(team);
+//        }
+//    }
+//
+//    @Test
+//    @DisplayName("Fetching all teams")
+//    void fetchAllTeams() {
+//        Team team = teamRepository.findById(testTeamId);
+//        given().header("Content-Type", "application/json")
+//                .when().get("/workstation/teams")
+//                .then().statusCode(200)
+//                .body("$.size()", equalTo(1))
+//                .body("[0].name", equalTo(team.getName()))
+//                .body("[0].defaultLocation", equalTo(team.getDefaultLocation()))
+//                .body("[0].product", equalTo(team.getProduct()));
+//    }
+//
+//    @Test
+//    @DisplayName("Fetching a specific team")
+//    void fetchSpecificTeam() {
+//        Team team = teamRepository.findById(testTeamId);
+//        given().header("Content-Type", "application/json")
+//                .when().get("/workstation/teams/{id}", testTeamId)
+//                .then().statusCode(200)
+//                .body("name", equalTo(team.getName()))
+//                .body("product", equalTo(team.getProduct()))
+//                .body("defaultLocation", equalTo(team.getDefaultLocation()));
+//
+//        given().header("Content-Type", "application/json")
+//                .when().get()
+//                .then().statusCode(404);
+//    }
+//
+//    @Test
+//    @DisplayName("Creating a team")
+//    void createTeam() {
+//        TeamRequest teamRequest = new TeamRequest("Default Team", "Product", "Location");
+//        TeamResponse teamResponse = new TeamResponse(null,"Default Team", "Location", "Product", null);
+//        TeamResponse response = given()
+//                .contentType(ContentType.JSON)
+//                .body(teamRequest)
+//                .when()
+//                .post("/workstation/teams")
+//                .then()
+//                .assertThat()
+//                .statusCode(RestResponse.StatusCode.CREATED)
+//                .extract().body().as(TeamResponse.class);
+//
+//        assertThat(response)
+//                .as("Team was created")
+//                .hasNoNullFieldsOrProperties()
+//                .usingRecursiveComparison()
+//                .ignoringFields("id", "memberList")
+//                .isEqualTo(teamResponse);
+//    }
+//
+//    @Test
+//    @DisplayName("Creating a team with blank values")
+//    void createTeamWithBlankValues() {
+//        TeamRequest teamRequest = new TeamRequest("", "", "");
+//        //TODO Criar class de excepção e mapear as respostas de erro para lá.
+//        given()
+//                .contentType(ContentType.JSON)
+//                .body(teamRequest)
+//                .when()
+//                .post("/workstation/teams")
+//                .then()
+//                .statusCode(RestResponse.StatusCode.BAD_REQUEST)
+//                .body("title", equalTo("Constraint Violation"))
+//                .body("violations[0].field", equalTo("create.teamRequest.product"))
+//                .body("violations[0].message", equalTo("must not be blank"))
+//                .body("violations[1].field", equalTo("create.teamRequest.name"))
+//                .body("violations[1].message", equalTo("must not be blank"));
+//    }
+//}
